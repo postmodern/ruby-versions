c88f8d0e8c93688652a1cc33e06efd934c756d82
Postmodern <postmodern.mod3@gmail.com>
2015-11-19 16:11:03 -0800

    Added an update.sh script.

---
+++ update.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ ! $# -eq 2 ]]; then
+	echo "usage: $0 [ruby|mruby|jruby|rubinius] [VERSION]"
+	exit -1
+fi
+
+ruby="$1"
+version="$2"
+dest="pkg"
+
+case "$ruby" in
+	ruby)
+		version_family="${version:0:3}"
+
+		exts=(tar.gz tar.bz2 tar.xz zip)
+		downloads_url="http://cache.ruby-lang.org/pub/ruby/"
+		;;
+	mruby)
+		exts=(tar.gz zip)
+		downloads_url="https://github.com/mruby/mruby/archive/"
+		;;
+	jruby)
+		exts=(tar.gz zip)
+		downloads_url="https://s3.amazonaws.com/jruby.org/downloads/"
+		;;
+	rubinius)
+		ruby="rbx"
+		exts=(tar.bz2)
+		downloads_url="http://releases.rubini.us/"
+		;;
+	*)
+		echo "$0: unknown ruby: $ruby" >&2
+		exit -1
+		;;
+esac
+
+mkdir -p "$dest"
+pushd "$dest" >/dev/null
+
+for ext in "${exts[@]}"; do
+	case "$ruby" in
+		ruby)
+			archive="ruby-${version}.${ext}"
+			url="$downloads_url/$version_family/$archive"
+			;;
+		mruby)
+			archive="mruby-${version}.${ext}"
+			url="$downloads_url/$version/$archive"
+			;;
+		jruby)
+			archive="jruby-bin-${version}.${ext}"
+			url="$downloads_url/$version/$archive"
+			;;
+		rubinius)
+			archive="rubinius-${version}.${ext}"
+			url="$downloads_url/$archive"
+			;;
+	esac
+
+	wget -O "$archive" "$url"
+
+	for algorithm in md5 sha1 sha256 sha512; do
+		${algorithm}sum "$archive" >> "../$ruby/checksums.$algorithm"
+	done
+done
+
+echo "$version" >> "../$ruby/versions.txt"
+
+popd >/dev/null
